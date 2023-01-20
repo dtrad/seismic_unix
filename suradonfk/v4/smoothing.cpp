1fc8c711ce39eee26b3d8322c71984b32d089b3b
Daniel Trad <danielotrad@gmail.com>
2023-01-20 15:54:44 -0700

    changed compiler and conv

--- suradonfk/v4/smoothing.cpp
+++ suradonfk/v4/smoothing.cpp
@@ -1,9 +1,21 @@
-/* Copyright (c) University of British Columbia, 1999.*/
-/* All rights reserved.                       */
-
-/* SURADTD:  $Date: June 1999  */
 #include "su.h"
-
+#include "cwp.h"
+#include "Complex.h"
+void conv(int lx, int ifx, float *x,
+	   int ly, int ify, float *y,
+	   int lz, int ifz, float *z){
+	int ilx=ifx+lx-1,ily=ify+ly-1,ilz=ifz+lz-1,i,j,jlow,jhigh;
+	float sum;
+	
+	x -= ifx;  y -= ify;  z -= ifz;
+	for (i=ifz; i<=ilz; ++i) {
+		jlow = i-ily;  if (jlow<ifx) jlow = ifx;
+		jhigh = i-ify;  if (jhigh>ilx) jhigh = ilx;
+		for (j=jlow,sum=0.0; j<=jhigh; ++j)
+			sum += x[j]*y[i-j];
+		z[i] = sum;
+	}
+}
 
 void smoothing(float *d,int nt,int nx,int nl,int nr,int flag)
 {
